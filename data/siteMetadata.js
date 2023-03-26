af331329ca538ac77cb44654fffe5f0910cda572
Sachin Tyagi <sachin.tyagi@gmail.com>
2023-03-26 12:55:02 +0530

    Updated the repo url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.satto.in',
+  siteRepo: 'https://github.com/sachintyagi22/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/sachin.png',
   socialBanner: '/static/images/twitter-card.png',
